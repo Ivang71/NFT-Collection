ed4c2a431ce93d96bebc76f33ae5fb9b5a417a98
Artem Tolochkov <artemtolv@gmail.com>
2022-07-06 10:09:47 +0300

    Update frontend url

--- hardhat-tutorial/constants/index.js
+++ hardhat-tutorial/constants/index.js
@@ -1,6 +1,6 @@
 // Address of the Whitelist Contract that you deployed
 const WHITELIST_CONTRACT_ADDRESS = '0xdabf101a2D874801940f0ed0135B8B59825334bC'
 // URL to extract Metadata for a Crypto Dev NFT
-const METADATA_URL = 'https://'
+const METADATA_URL = 'https://nft-collection-iota-eight.vercel.app/api/'
 
 module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
