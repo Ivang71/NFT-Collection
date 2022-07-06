1486ce7e80f77a75bce2b12ad02008120420b6c8
Artem Tolochkov <artemtolv@gmail.com>
2022-07-06 09:57:29 +0300

    init

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,6 @@
+// Address of the Whitelist Contract that you deployed
+const WHITELIST_CONTRACT_ADDRESS = '0xdabf101a2D874801940f0ed0135B8B59825334bC'
+// URL to extract Metadata for a Crypto Dev NFT
+const METADATA_URL = 'https://'
+
+module.exports = { WHITELIST_CONTRACT_ADDRESS, METADATA_URL }
